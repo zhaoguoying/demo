176cbe6a153f9e069ec51af0f1656dbaeaac8d79
zhaoguoying <972445978@qq.com>
2018-12-31 01:45:36 +0800

    项目第一次 1.0.0.1-完成model

---
+++ src/main/java/com/example/demo/model/Archive.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+/*
+ * 作者：赵国应
+ * 时间：2018-12-31 01:55
+ * 描述：文章归档
+ */
+@Data
+public class Archive {
+    private int id;
+
+    /**
+     * 归档日期
+     */
+    private String archiveName;
+
+
+}
