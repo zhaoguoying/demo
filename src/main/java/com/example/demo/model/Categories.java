20475d194c699ae826d2ca9a96fe69a2d63e7a11
zhaoguoying <972445978@qq.com>
2018-12-31 01:37:28 +0800

    项目第一次 1.0.0

---
+++ src/main/java/com/example/demo/model/Categories.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+/*
+ * 作者：赵国应
+ * 时间：2018-12-31 01:36
+ * 描述：文章分类
+ */
+
+import lombok.Data;
+
+@Data
+public class Categories {
+    private int id;
+
+    private String categoryName;
+}
+
