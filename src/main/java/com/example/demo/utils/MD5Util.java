d47927ad7a0cc6359aef33fcb432d3a92e5df06b
zhaoguoying <972445978@qq.com>
2018-12-31 15:05:37 +0800

    项目第一次 1.0.0.1-完成utils设计

---
+++ src/main/java/com/example/demo/utils/MD5Util.java
@@ -0,0 +1,42 @@
+package com.example.demo.utils;
+
+/*
+ * 作者：赵国应
+ * 时间：2018-12-31 15:25
+ * 描述：MD5加密工具
+ */
+
+import java.security.MessageDigest;
+
+public class MD5Util {
+
+    private static final String SALT = "tamboo";
+
+    public String encode(String password) {
+        password = password + SALT;
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        char[] charArray = password.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++){
+            byteArray[i] = (byte) charArray[i];
+        }
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuffer hexValue = new StringBuffer();
+        for (byte md5Byte : md5Bytes) {
+            int val = ((int) md5Byte) & 0xff;
+            if (val < 16) {
+                hexValue.append("0");
+            }
+
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+    }
+
+}
